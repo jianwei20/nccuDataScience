c0d42aa35331af28deb13d29f18604433939ffc4
MinRong Li <ADT101106@gm.ntcu.edu.tw>
2017-03-13 19:43:37 +0800

    version 2

--- hw1/hw1_105753040.R
+++ hw1/hw1_105753040.R
@@ -1,16 +1,31 @@
-########################
-# homework1 example
-########################
-
 args = commandArgs(trailingOnly=TRUE)
 if (length(args)==0) {
   stop("USAGE: Rscript hw1_105753040.R input", call.=FALSE)
-} else if (length(args)==1) {
+}else if (length(args)==1) {
   i_f <- args[1]
+  #read csv data
+  data <- read.table(paste("./",i_f,sep=''),header=TRUE,sep=",")
+  weight <- round(max(data[2]),2)
+  height <- round(max(data[3]),2)
+  #output csv data
+  set <- gsub(".csv","",i_f)
+  df <- data.frame(set,weight,height,row.names = NULL)
+  write.table(df,file="result.csv",sep=",",row.names = FALSE)
+}else if (length(args)==4){
+  inFile <- args[2]
+  outFile <- args[4]
+  #the second argument is -out
+  if(args[1]=="-out"){
+    inFile <- args[4]
+    outFile <- args[2]
+  }
+  #read csv data
+  data <- read.table(paste("./",inFile,sep=''),header=TRUE,sep=",")
+  weight <- round(max(data[2]),2)
+  height <- round(max(data[3]),2)
+  #output csv data
+  set <- gsub(".csv","",inFile)
+  df <- data.frame(set,weight,height,row.names = NULL)
+  write.table(df,file=outFile,sep=",",row.names = FALSE)
 }
-data <- read.table(paste("./",args[2],sep=''),header=TRUE,sep=",")
-weight <- round(max(data[2]),2)
-height <- round(max(data[3]),2)
-set <- gsub(".csv","",args[2])
-df <- data.frame(set,weight,height,row.names = NULL)
-write.table(df,file="result.csv",sep=",",row.names = FALSE)
+
